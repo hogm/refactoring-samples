6d06353f8a8743bbe683a51919d917560632585c
ogami <h-ogami@winas.jp>
2018-01-15 16:30:11 +0900

    Readme追加、ファイル名整理

--- Java/level-000.java
+++ Java/level-000.java
@@ -7,25 +7,30 @@ public class Main {
 
         while(true) {
             a = a + 1;
-            b = a % 3;
-            c = a % 5;
-            d = a % 15;
 
             if (a > 100) {
                 break;
             }
+
+            b = a % 3;
+            c = a % 5;
+            d = a % 15;
+
             if (d == 0) {
                 System.out.println("FizzBuzz");
                 continue
             }
+
             if (b == 0) {
                 System.out.println("Fizz");
                 continue
             }
+
             if (c == 0) {
                 System.out.println("Buzz");
                 continue
             }
+
             System.out.println(a);
         }
     }
