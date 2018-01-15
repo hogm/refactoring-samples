19819661d6e6c802cf50d1efd58eccc55ab36960
ogami <h-ogami@winas.jp>
2018-01-15 20:12:12 +0900

    600を追加

---
+++ Java/level-600.java
@@ -0,0 +1,27 @@
+public class Main {
+    private static final int LOOP_COUNT = 100;
+
+    public static void main(String[] args) {
+        for (int i=1; i<=LOOP_COUNT; i++) {
+            System.out.println(toFizzBuzzString(i));
+        }
+    }
+
+    private static String toFizzBuzzString(int number) {
+        String value = isFizz(number) ? "Fizz" : "";
+        value += isBuzz(number) ? "Buzz" : "";
+        return value.isEmpty() ? String.valueOf(number) : value;
+    }
+
+    private static boolean isFizzBuzz(int number) {
+        return isFizz(number) && isBuzz(number);
+    }
+
+    private static boolean isFizz(int number) {
+        return number % 3 == 0;
+    }
+
+    private static boolean isBuzz(int number) {
+        return number % 5 == 0;
+    }
+}
